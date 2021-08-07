f9f56100c02542a8b2828cd8097676dee1c76fe8
Jefferson Luís Nascimento <je.luis.nascimento@hotmail.com>
2021-08-07 13:42:16 -0300

    Implement NaturalPersonMap

---
+++ MyContacts.Backend/MyContacts.Repository/Maps/BaseEntityMap.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyContacts.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyContacts.Repository.Maps
+{
+    public class BaseEntityMap<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : BaseEntity
+    {
+        private readonly string _tableName;
+
+        public BaseEntityMap()
+        {
+            _tableName = string.Empty;
+        }
+
+        public BaseEntityMap(string tableName)
+        {
+            _tableName = tableName;
+        }
+
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            if(!string.IsNullOrWhiteSpace(_tableName))
+            {
+                builder.ToTable(_tableName);
+            }
+
+            builder.HasKey(entity => entity.Id);
+            builder.Property(entity => entity.Id).HasColumnName("id");
+            builder.Property(entity => entity.CreatedIn).HasColumnName("created_in").IsRequired();
+        }
+    }
+}
---
+++ MyContacts.Backend/MyContacts.Repository/Maps/NaturalPersonMap.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyContacts.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyContacts.Repository.Maps
+{
+    public class NaturalPersonMap : BaseEntityMap<NaturalPerson>
+    {
+        public NaturalPersonMap() 
+            : base("natural_person") 
+        {
+
+        }
+
+        public override void Configure(EntityTypeBuilder<NaturalPerson> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(person => person.Name.FullName).HasColumnName("name").HasMaxLength(200).IsRequired();
+            builder.Property(person => person.CPF.Number).HasColumnName("cpf").HasMaxLength(14).IsRequired();
+            builder.Property(person => person.Birthday).HasColumnName("birthday").IsRequired();
+            builder.Property(person => person.Gender).HasColumnName("gender").IsRequired();
+        }
+    }
+}
