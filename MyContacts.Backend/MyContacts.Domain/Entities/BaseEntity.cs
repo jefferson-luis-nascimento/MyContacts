c024aa03f6e85796bf7ef36601da4e190a03ae2a
Jefferson Luís Nascimento <je.luis.nascimento@hotmail.com>
2021-08-07 11:43:05 -0300

    Create BaseEntity

---
+++ MyContacts.Backend/MyContacts.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyContacts.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedIn { get; set; }
+        public DateTime? UpdatedIn { get; set; }
+    }
+}
