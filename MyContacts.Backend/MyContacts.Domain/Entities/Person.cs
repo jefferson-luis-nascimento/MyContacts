45c90757249c93e9ce1036181b9c4c95689a4a5a
Jefferson Luís Nascimento <je.luis.nascimento@hotmail.com>
2021-08-07 11:49:47 -0300

    Create Person entity

---
+++ MyContacts.Backend/MyContacts.Domain/Entities/Person.cs
@@ -0,0 +1,14 @@
+﻿using MyContacts.Domain.VOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyContacts.Domain.Entities
+{
+    public abstract class Person : BaseEntity
+    {
+        public Address Address { get; set; }
+    }
+}
